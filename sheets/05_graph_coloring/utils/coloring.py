6e282713cdedcd6506d7b26a294e3009770582ac
simonlsgr <simon.laschinger@web.de>
2025-12-02 13:05:30 +0100

    fix: check if node is actually colored

--- sheets/05_graph_coloring/utils/coloring.py
+++ sheets/05_graph_coloring/utils/coloring.py
@@ -2,6 +2,6 @@ import networkx as nx
 
 def is_valid_coloring(graph: nx.Graph):
     for u, v in graph.edges:
-        if graph.nodes[u]["color"] == graph.nodes[v]["color"]:
+        if graph.nodes[u]["color"] == graph.nodes[v]["color"] and graph.nodes[v]["color"] != -1:
             return False
     return True
